da95733094ed006fca4e2cb3a08e996b8775fb9c
RasenBunkai <l19530391@cancun.tecnm.mx>
2023-11-11 21:03:51 -0500

    ACTUALIZACION

---
+++ assets/js/app.js
@@ -0,0 +1,109 @@
+/* FUNCION DE MODAL AMERICAS */
+let ModalAmerica = document.getElementById("btn-america")
+ModalAmerica.addEventListener("click",ShowAmerica)
+
+let CloseModalAmerica = document.getElementById("btm-closeAmerica")
+CloseModalAmerica.addEventListener("click",CloseAmerica)
+
+function ShowAmerica(){
+    let MAmerica = document.getElementById("ModalAmerica")
+    MAmerica.classList.add("d-flex")
+    console.log("showAmerica")
+}
+
+function CloseAmerica(){
+    let MAmerica = document.getElementById("ModalAmerica")
+    MAmerica.classList.remove("d-flex")
+}
+
+/* MODAL PUERTO CANCUN*/
+let ModalPuerto = document.getElementById("btn-puerto")
+ModalPuerto.addEventListener("click",ShowPuerto)
+
+let CloseModalPuerto = document.getElementById("btm-closePuerto")
+CloseModalPuerto.addEventListener("click",ClosePuerto)
+
+function ShowPuerto(){
+    let MPuerto = document.getElementById("ModalPuerto")
+    MPuerto.classList.add("d-flex")
+    console.log("showPuerto")
+}
+
+function ClosePuerto(){
+    let MPuerto = document.getElementById("ModalPuerto")
+    MPuerto.classList.remove("d-flex")
+    console.log("ClosePuerto")
+}
+
+/* MODAL PLAZA FORUM */
+let ModalForum = document.getElementById("btn-forum")
+ModalForum.addEventListener("click",ShowForum)
+
+let CloseModalForum = document.getElementById("btm-closeForum")
+CloseModalForum.addEventListener("click", CloseForum)
+
+function ShowForum(){
+    let MForum = document.getElementById("ModalForum")
+    MForum.classList.add("d-flex")
+    console.log("showforum") /* aqui */
+}
+
+function CloseForum(){
+    let MForum = document.getElementById("ModalForum")
+    MForum.classList.remove("d-flex")
+    console.log("CloseForum") /* aqui */
+}
+
+/* MODAL PLAZA ISLA */
+let ModalIsla = document.getElementById("btn-isla")
+ModalIsla.addEventListener("click",ShowIsla)
+
+let CloseModalIsla = document.getElementById("btm-closeIsla")
+CloseModalIsla.addEventListener("click",CloseIsla)
+
+function ShowIsla(){
+    let MIsla = document.getElementById("ModalIsla")
+    MIsla.classList.add("d-flex")
+    console.log("showIsla")
+}
+
+function CloseIsla(){
+    let MIsla = document.getElementById("ModalIsla")
+    MIsla.classList.remove("d-flex")
+}
+
+/* MODAL PLAZA KUKULCAN */
+let ModalKukulcan = document.getElementById("btn-kukulcan")
+ModalKukulcan.addEventListener("click",ShowKukulcan)
+
+let CloseModalKukulcan = document.getElementById("btm-closeKukulcan")
+CloseModalKukulcan.addEventListener("click",CloseKukulcan)
+
+function ShowKukulcan(){
+    let MKukulcan = document.getElementById("ModalKukulcan")
+    MKukulcan.classList.add("d-flex")
+    console.log("showKukulcan")
+}
+
+function CloseKukulcan(){
+    let MKukulcan = document.getElementById("ModalKukulcan")
+    MKukulcan.classList.remove("d-flex")
+}
+
+/* MODAL PLAZA CARACOL */
+let ModalCaracol = document.getElementById("btn-caracol")
+ModalCaracol.addEventListener("click",ShowCaracol)
+
+let CloseModalCaracol = document.getElementById("btm-closeCaracol")
+CloseModalCaracol.addEventListener("click", CloseCaracol)
+
+function ShowCaracol(){
+    let MCaracol = document.getElementById("ModalCaracol")
+    MCaracol.classList.add("d-flex")
+    console.log("showCaracol")
+}
+
+function CloseCaracol(){
+    let MCaracol = document.getElementById("ModalCaracol")
+    MCaracol.classList.remove("d-flex")
+}
